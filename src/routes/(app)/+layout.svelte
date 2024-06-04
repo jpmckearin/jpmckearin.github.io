b34851a4d6fbefc628a44a6de1b67942a0502cd4
Josh McKearin <josh@mckearin.codes>
2024-06-04 16:52:33 -0500

    feat: initial profile

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -7,13 +7,4 @@
 	<title>Josh McKearin</title>
 </svelte:head>
 
-<header
-	class="sticky p-2 top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
->
-	<div class="flex">
-		<a href="/" class="font-bold text-lg mix-blend-difference">
-			{env.PUBLIC_PROJECT_NAME}
-		</a>
-	</div>
-</header>
 {@render children()}
